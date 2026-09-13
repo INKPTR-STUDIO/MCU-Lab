2bcec7d8db10ccb5e0663920b9a3d6235603188b
指针砚工作室 <154938912+INKPTR-STUDIO@users.noreply.github.com>
2026-09-13 23:53:55 +0800

    Update INKPTR_I2C.h

--- I2C/INKPTR_I2C.h
+++ I2C/INKPTR_I2C.h
@@ -1,19 +1,22 @@
 #ifndef INKPTR_I2C_H
 #define INKPTR_I2C_H
 
-#include "ch32v00x.h"
-
+/*================================================================================================*/
+// Part 1: Adjustable parameter macro definition
 // GPIO mapping register
 #define INKPTR_I2C_RCC_APB2Periph  (RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD)
 // GPIO speed
 #define INKPTR_I2C_GPIO_Speed      GPIO_Speed_50MHz
-// SCL
+// SCL pin
 #define INKPTR_I2C_SCL_Port        GPIOC
 #define INKPTR_I2C_SCL_Pin         GPIO_Pin_0
-// SDA
+// SDA pin
 #define INKPTR_I2C_SDA_Port        GPIOD
 #define INKPTR_I2C_SDA_Pin         GPIO_Pin_0
 
+/*================================================================================================*/
+// Part 2: Library and interface function declarations
+#include "ch32v00x.h"
 void    INKPTR_I2C_Init(void);
 void    INKPTR_I2C_Start(void);
 void    INKPTR_I2C_Stop(void);
