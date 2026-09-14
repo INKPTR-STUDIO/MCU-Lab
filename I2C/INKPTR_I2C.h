492abe19ada37becf2c1aba12bf94e4cd430309d
指针砚工作室 <154938912+INKPTR-STUDIO@users.noreply.github.com>
2026-09-14 19:23:28 +0800

    Update INKPTR_I2C.h

--- I2C/INKPTR_I2C.h
+++ I2C/INKPTR_I2C.h
@@ -2,7 +2,18 @@
 #define INKPTR_I2C_H
 
 /*================================================================================================*/
-// Part 1: Adjustable parameter macro definition
+// Part 1: Library and interface function declarations
+#include "ch32v00x.h"
+void    INKPTR_I2C_Init(void);
+void    INKPTR_I2C_Start(void);
+void    INKPTR_I2C_Stop(void);
+void    INKPTR_I2C_SendByte(uint8_t SendByte);
+uint8_t INKPTR_I2C_ReceiveACK(void);
+uint8_t INKPTR_I2C_ReceiveByte(void);
+void    INKPTR_I2C_SendACK(uint8_t SendACK);
+
+/*================================================================================================*/
+// Part 2: Adjustable parameter macro definition
 // GPIO mapping register
 #define INKPTR_I2C_RCC_APB2Periph  (RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD)
 // GPIO speed
@@ -13,16 +24,5 @@
 // SDA pin
 #define INKPTR_I2C_SDA_Port        GPIOD
 #define INKPTR_I2C_SDA_Pin         GPIO_Pin_0
-
-/*================================================================================================*/
-// Part 2: Library and interface function declarations
-#include "ch32v00x.h"
-void    INKPTR_I2C_Init(void);
-void    INKPTR_I2C_Start(void);
-void    INKPTR_I2C_Stop(void);
-void    INKPTR_I2C_SendByte(uint8_t SendByte);
-uint8_t INKPTR_I2C_ReceiveACK(void);
-uint8_t INKPTR_I2C_ReceiveByte(void);
-void    INKPTR_I2C_SendACK(uint8_t SendACK);
 
 #endif
