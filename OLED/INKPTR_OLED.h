d2bb0d0e092f303ce4a48decd866d3bc0ea71f2b
指针砚工作室 <154938912+INKPTR-STUDIO@users.noreply.github.com>
2026-09-14 18:25:15 +0800

    Update INKPTR_OLED.h

--- OLED/INKPTR_OLED.h
+++ OLED/INKPTR_OLED.h
@@ -1,13 +1,16 @@
 #ifndef INKPTR_OLED_H
 #define INKPTR_OLED_H
 
-#include "ch32v00x.h"
-
+/*================================================================================================*/
+// Part 1: Adjustable parameter macro definition
 #define INKPTR_OLED_Model 0
 // 128*64	> 0
 // 128*32	> 1
 // 72*40	> 2
 
+/*================================================================================================*/
+// Part 2: Fixed parameter macro definitions and enumerations
+#define INKPTR_OLED_ADD 0x78
 typedef enum {INKPTR_OLED_AddressingMode_HorizontalMode = 0, INKPTR_OLED_AddressingMode_VerticalMode, INKPTR_OLED_AddressingMode_PageMode} INKPTR_OLED_AddressingMode;
 typedef enum {INKPTR_OLED_SetMode_Brightness = 0, INKPTR_OLED_SetMode_X_FlipMode, INKPTR_OLED_SetMode_Y_FlipMode, INKPTR_OLED_SetMode_ColorMode, INKPTR_OLED_SetMode_RollSwitch, INKPTR_OLED_SetMode_ShowSwitch} INKPTR_OLED_SetMode;
 typedef enum {INKPTR_OLED_SetMode_X_Flip_Mirror = 0, INKPTR_OLED_SetMode_X_Flip_Normal} INKPTR_OLED_SetMode_X_Flip;
@@ -21,6 +24,9 @@ typedef enum
     INKPTR_OLED_RollMode_Left_1 = 0x80, INKPTR_OLED_RollMode_Left_2 = 0x81, INKPTR_OLED_RollMode_Left_3 = 0x82, INKPTR_OLED_RollMode_Left_4 = 0x83, INKPTR_OLED_RollMode_Left_5 = 0x84, INKPTR_OLED_RollMode_Left_6 = 0x85, INKPTR_OLED_RollMode_Left_7 = 0x86, INKPTR_OLED_RollMode_Left_8 = 0x87
 } INKPTR_OLED_RollMode;
 
+/*================================================================================================*/
+// Part 3: Library and interface function declarations
+#include "ch32v00x.h"
 void    INKPTR_OLED_Init(INKPTR_OLED_AddressingMode AddressingMode, INKPTR_OLED_SetMode_X_Flip X_FlipMode, INKPTR_OLED_SetMode_Y_Flip Y_FlipMode, INKPTR_OLED_SetMode_Color ColorMode, uint8_t Brightness);
 void    INKPTR_OLED_Set(INKPTR_OLED_SetMode SetMode, uint8_t Dat);
 void    INKPTR_OLED_Brush(uint8_t Page_Begin, uint8_t Page_End, uint8_t List_Begin, uint8_t List_End, uint8_t Style_Byte);
